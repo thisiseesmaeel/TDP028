18ef588a848f3ee184acdd2dc55055502cdd79cd
Ismail Safwat <ismail@hey.com>
2021-11-02 09:39:47 +0100

    Klar med sign up sidan

--- Informera/app/src/main/java/com/example/informera/MainActivity.java
+++ Informera/app/src/main/java/com/example/informera/MainActivity.java
@@ -2,59 +2,31 @@ package com.example.informera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-private FirebaseAnalytics mFirebaseAnalytics;
-private FirebaseAuth mAuth;
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        // Build a GoogleSignInClient with the options specified by gso.
-        Object mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-
-
-        mAuth = FirebaseAuth.getInstance();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = findViewById(R.id.signupButton1);
+        register.setOnClickListener(this);
+
     }
 
-    protected void onStart() {
-
-        super.onStart();
-
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(GoogleSignInAccount account) {
-    }
-
-    private void updateUI(FirebaseUser account) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signupButton1:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
---
+++ Informera/app/src/main/java/com/example/informera/RegisterUser.java
@@ -0,0 +1,88 @@
+package com.example.informera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView create_acc_text, already_registered_text, signupButton2;
+    private EditText editTextTextPersonName, editTextTextEmailAddress, editTextTextPassword;
+    private ProgressBar progressBar2;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //create_acc_text = (TextView) findViewById(R.id.create_acc_text);
+
+        already_registered_text = findViewById(R.id.loginButton);
+        already_registered_text.setOnClickListener(this);
+
+        signupButton2 = findViewById(R.id.signupButton2);
+        signupButton2.setOnClickListener(this);
+
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+
+        progressBar2 = findViewById(R.id.progressBar2);
+
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.already_registered_text:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.signupButton2:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String name = editTextTextPersonName.getText().toString().trim();
+        String email = editTextTextEmailAddress.getText().toString().trim();
+        String password = editTextTextPassword.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            editTextTextPersonName.setError("Make sure you have entered your name!");
+            editTextTextPersonName.requestFocus();
+        }
+        if (email.isEmpty()) {
+            editTextTextEmailAddress.setError("Make sure you have entered your email address!");
+            editTextTextEmailAddress.requestFocus();
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextTextEmailAddress.setError("Please enter a valid email address!");
+            editTextTextEmailAddress.requestFocus();
+        }
+        if (password.isEmpty()) {
+            editTextTextPassword.setError("Make sure you have entered your password!");
+            editTextTextPassword.requestFocus();
+        }
+        if (password.length() < 6) {
+            editTextTextPassword.setError("Password must be minimum 6 characters!");
+            editTextTextPassword.requestFocus();
+        }
+    }
+}
+
