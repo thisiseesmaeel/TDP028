67e7d0a4930d35071def71ab3148fc6c164fbe9d
Ismail Safwat <ismail@hey.com>
2022-02-08 18:05:31 +0100

    resumed coding, started work on a new app (Kviz), dependencies

---
+++ Kviz/app/src/main/java/com/example/kviz/Fragment/StartFragment.java
@@ -0,0 +1,30 @@
+package com.example.kviz.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kviz.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StartFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StartFragment extends Fragment {
+
+    public StartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.startfragment, container, false);
+    }
+}
